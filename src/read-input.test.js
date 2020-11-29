7a37b0853fe15c91b72cd6231ba051dfd5f86468
Jim Newbery <james@tinnedfruit.com>
2020-11-29 16:57:10 +0000

    Enhanced readInput tests

--- src/read-input.test.js
+++ src/read-input.test.js
@@ -7,4 +7,16 @@ test('readInput throws error if no file exists', () => {
   const mycb = jest.fn();
   expect(() => readInput(1, mycb)).toThrow(Error);
   expect(mycb).not.toHaveBeenCalled();
+  expect(fs.readFile.mock.calls[0][0]).toBe('./data/day1.txt');
+});
+
+test('readInput calls callback with file data if successful', () => {
+  const buffer = {
+    toString: jest.fn().mockReturnValue('file contents'),
+  };
+  fs.readFile.mockImplementation((path, cb) => cb(null, buffer));
+  const mycb = jest.fn();
+  readInput(1, mycb);
+  expect(fs.readFile.mock.calls[0][0]).toBe('./data/day1.txt');
+  expect(mycb.mock.calls[0][0]).toBe('file contents');
 });
