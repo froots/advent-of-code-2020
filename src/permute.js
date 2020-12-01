fd34ca845e666570d6a8ec8b3c4e93608842d6c1
Jim Newbery <james@tinnedfruit.com>
2020-12-01 17:30:21 +0000

    Permute with any sized permutation

--- src/permute.js
+++ src/permute.js
@@ -2,16 +2,24 @@
  * Permutes an array into 2 item arrays of permutions.
  * At some point this could be adapted to take any size of permutation chunk
  * @param {Array} items The array of items to create permutations from
+ * @param {Number} size The size of each permutation. Defaults to 2.
  */
 
-function permute(items) {
-  if (items.length < 2) {
+function permute(items, size = 2) {
+  if (size > items.length || size < 1) {
     return [];
+  }
+  if (size === 1) {
+    return items.map((item) => [item]);
   }
 
   const [head, ...rest] = items;
 
-  return [...rest.map((r) => [head, r]), ...permute(rest)];
+  const restPerms = permute(rest, size - 1);
+  return [
+    ...restPerms.map((perms) => [head, ...perms]),
+    ...permute(rest, size),
+  ];
 }
 
 module.exports = permute;
--- src/permute.test.js
+++ src/permute.test.js
@@ -1,25 +1,86 @@
 const permute = require('./permute');
 
-test('permute with zero items returns empty array', () => {
+test('permute with zero size returns empty array', () => {
   const input = [];
-  expect(permute(input)).toEqual([]);
+  expect(permute(input, 0)).toEqual([]);
 });
 
-test('permute with items count less than size of permutation chunks returns empty array', () => {
+test('permute with size one and empty items returns empty array', () => {
+  const input = [];
+  expect(permute(input, 1)).toEqual([]);
+});
+
+test('permute with size one and one item returns permutation', () => {
+  const input = [1];
+  expect(permute(input, 1)).toEqual([[1]]);
+});
+
+test('permute with size one and three items returns permutations', () => {
+  const input = [1, 2, 3];
+  expect(permute(input, 1)).toEqual([[1], [2], [3]]);
+});
+
+test('permute with size two and one item returns empty', () => {
   const input = [1];
   expect(permute(input)).toEqual([]);
 });
 
-test('permute with single chunk returns chunk', () => {
+test('permute with size two and two items returns one permutation', () => {
   const input = [1, 2];
   expect(permute(input)).toEqual([[1, 2]]);
 });
 
-test('permute with three items and size of 2 returns 3 permutations', () => {
+test('permute with size two and three items returns three permutations', () => {
   const input = [1, 2, 3];
   expect(permute(input)).toEqual([
     [1, 2],
     [1, 3],
     [2, 3],
+  ]);
+});
+
+test('permute with size three and three items returns one permutation', () => {
+  const input = [1, 2, 3];
+  const actual = permute(input, 3);
+  expect(actual).toEqual([[1, 2, 3]]);
+});
+
+test('permute with size three and four items returns permutations', () => {
+  const input = [1, 2, 3, 4];
+  const actual = permute(input, 3);
+  expect(actual).toEqual([
+    [1, 2, 3],
+    [1, 2, 4],
+    [1, 3, 4],
+    [2, 3, 4],
+  ]);
+});
+
+test('permute with size three and five items returns permutations', () => {
+  const input = [1, 2, 3, 4, 5];
+  const actual = permute(input, 3);
+  expect(actual).toEqual([
+    [1, 2, 3],
+    [1, 2, 4],
+    [1, 2, 5],
+    [1, 3, 4],
+    [1, 3, 5],
+    [1, 4, 5],
+    [2, 3, 4],
+    [2, 3, 5],
+    [2, 4, 5],
+    [3, 4, 5],
+  ]);
+});
+
+test('permute with size four and five items returns permutations', () => {
+  const input = [1, 2, 3, 4, 5];
+  const actual = permute(input, 4);
+  expect(actual).toEqual([
+    [1, 2, 3, 4],
+    [1, 2, 3, 5],
+    [1, 2, 4, 5],
+    [1, 3, 4, 5],
+    [2, 3, 4, 5],
   ]);
 });
