5f5d288efa9c6b251b51b97681877b3057c6689a
Jim Newbery <james@tinnedfruit.com>
2020-12-04 11:34:58 +0000

    Refactor parser to create numbers and height object

--- src/day4/parse-input.js
+++ src/day4/parse-input.js
@@ -1,7 +1,23 @@
 function parsePassportInput(passportInput) {
-  return Object.fromEntries(
+  const passport = Object.fromEntries(
     passportInput.split(/\s+/).map((pair) => pair.split(':'))
   );
+
+  ['iyr', 'eyr', 'byr'].forEach((k) => {
+    if (passport[k]) {
+      passport[k] = Number(passport[k]);
+    }
+  });
+
+  if (passport.hgt) {
+    const [, value, units] = passport.hgt.match(/^(\d+)(\w+)$/);
+    passport.hgt = {
+      value: Number(value),
+      units,
+    };
+  }
+
+  return passport;
 }
 
 function parseInput(input) {
