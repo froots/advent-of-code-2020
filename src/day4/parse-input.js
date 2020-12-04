81830674cf9cae16885cce04f22ef9e31b6089a2
Jim Newbery <james@tinnedfruit.com>
2020-12-04 10:50:37 +0000

    Day 4 input parser

---
+++ src/day4/parse-input.js
@@ -0,0 +1,11 @@
+function parsePassportInput(passportInput) {
+  return Object.fromEntries(
+    passportInput.split(/\s+/).map((pair) => pair.split(':')),
+  );
+}
+
+function parseInput(input) {
+  return input.split('\n\n').map(parsePassportInput);
+}
+
+module.exports = parseInput;
