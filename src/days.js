9374987964f52c36e2442698cfbebcd52d722cbd
Jim Newbery <james@tinnedfruit.com>
2020-12-08 16:33:05 +0000

    Simplify days

--- src/days.js
+++ src/days.js
@@ -1,13 +1,11 @@
-const day0 = require('./day0');
-const day1 = require('./day1');
-const day2 = require('./day2');
-const day3 = require('./day3');
-const day4 = require('./day4');
-const day5 = require('./day5');
-const day6 = require('./day6');
-const day7 = require('./day7');
-const day8 = require('./day8');
-
-const days = [day0, day1, day2, day3, day4, day5, day6, day7, day8];
-
-module.exports = days;
+module.exports = [
+  require('./day0'),
+  require('./day1'),
+  require('./day2'),
+  require('./day3'),
+  require('./day4'),
+  require('./day5'),
+  require('./day6'),
+  require('./day7'),
+  require('./day8'),
+];
