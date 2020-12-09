0f1a8e84c51b2b0b65790407a227bd823e4f6e54
Jim Newbery <james@tinnedfruit.com>
2020-12-09 18:07:43 +0000

    Bit o' refactor

--- src/day9/part1.js
+++ src/day9/part1.js
@@ -1,18 +1,17 @@
 const permute = require('../permute');
 
 function part1(input, preambleSize) {
-  return input.find(noPrecedingSumPair(preambleSize));
+  return input.find(noPrecedingPairSumsToTarget(preambleSize));
 }
 
-function noPrecedingSumPair(preambleSize) {
-  // return true from this function if there are no pairs in the preceding
-  // preambleSize items that sum to n
+function noPrecedingPairSumsToTarget(preambleSize) {
   return function (n, i, list) {
     if (i < preambleSize) {
       return false;
     }
-    const pairs = permute(list.slice(i - preambleSize, i));
-    return !pairs.find(([a, b]) => a + b === n);
+    return !permute(list.slice(i - preambleSize, i)).find(
+      ([a, b]) => a + b === n
+    );
   };
 }
 
