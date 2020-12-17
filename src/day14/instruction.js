b55ca9905df9091dc97bf5b510a371b43f97bdcb
Jim Newbery <james@tinnedfruit.com>
2020-12-17 12:54:14 +0000

    Bit of function extraction

--- src/day14/instruction.js
+++ src/day14/instruction.js
@@ -23,52 +23,68 @@ function parse(line) {
   }
 }
 
+function setMaskV1(state, mask) {
+  return {
+    mask,
+    maskOn: BigInt(`0b${mask.replace(/X/g, '0')}`),
+    maskOff: BigInt(`0b${mask.replace(/X/g, '1')}`),
+    memory: state.memory,
+  };
+}
+
+function writeV1(state, address, value) {
+  state.memory.set(address, (value | state.maskOn) & state.maskOff);
+  return { ...state };
+}
+
 function executeV1(state, { operation, args }) {
   switch (operation) {
     case Operations.SET_MASK:
-      return {
-        mask: args[0],
-        maskOn: BigInt(`0b${args[0].replace(/X/g, '0')}`),
-        maskOff: BigInt(`0b${args[0].replace(/X/g, '1')}`),
-        memory: state.memory,
-      };
+      return setMaskV1(state, args[0], args[1]);
     case Operations.WRITE:
-      state.memory.set(args[0], (args[1] | state.maskOn) & state.maskOff);
-      return { ...state };
+      return writeV1(state, args[0], args[1]);
     default:
       throw new Error('Unknown operation');
   }
 }
 
+function setMaskV2(state, rawMask) {
+  const floating = [
+    ...rawMask.split('').reverse().join('').matchAll(/X/g),
+  ].map(({ index }) => BigInt(2 ** index));
+  const mask = BigInt(`0b${rawMask.replace(/X/g, '0')}`);
+
+  return {
+    rawMask,
+    mask,
+    floating,
+    floatingTotal: floating.reduce(sum, 0n),
+    memory: state.memory,
+  };
+}
+
+function writeV2(state, address, value) {
+  const base = (state.mask | address) & ~state.floatingTotal;
+  const permutations = Array.from(
+    Array(2 ** state.floating.length).keys()
+  ).map((n) =>
+    n.toString(2).padStart(state.floating.length, '0').split('').map(BigInt)
+  );
+  const masks = permutations.map((perm) =>
+    perm.map((bit, i) => bit * state.floating[i]).reduce(sum)
+  );
+  masks.forEach((mask) => {
+    state.memory.set(base | mask, value);
+  });
+  return { ...state };
+}
+
 function executeV2(state, { operation, args }) {
   switch (operation) {
     case Operations.SET_MASK:
-      const floating = [
-        ...args[0].split('').reverse().join('').matchAll(/X/g),
-      ].map(({ index }) => BigInt(2 ** index));
-      const mask = BigInt(`0b${args[0].replace(/X/g, '0')}`);
-
-      return {
-        rawMask: args[0],
-        mask,
-        floating,
-        floatingTotal: floating.reduce(sum, 0n),
-        memory: state.memory,
-      };
+      return setMaskV2(state, args[0]);
     case Operations.WRITE:
-      const base = (state.mask | args[0]) & ~state.floatingTotal;
-      const permutations = Array.from(
-        Array(2 ** state.floating.length).keys()
-      ).map((n) =>
-        n.toString(2).padStart(state.floating.length, '0').split('').map(BigInt)
-      );
-      const masks = permutations.map((perm) =>
-        perm.map((bit, i) => bit * state.floating[i]).reduce(sum)
-      );
-      masks.forEach((mask) => {
-        state.memory.set(base | mask, args[1]);
-      });
-      return { ...state };
+      return writeV2(state, args[0], args[1]);
     default:
       throw new Error('Unrecognised operation');
   }
