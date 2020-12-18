3676adf6630010bce30107b004d8495b79961ae7
Jim Newbery <james@tinnedfruit.com>
2020-12-18 17:52:36 +0000

    4D cube tests

--- src/day17/cube.test.js
+++ src/day17/cube.test.js
@@ -46,3 +46,39 @@ describe('3 dimensional', () => {
     expect(cube.activeCount).toBe(38);
   });
 });
+
+describe('4 dimensional', () => {
+  test('Empty cube constructor', () => {
+    const cube = new Cube(4);
+    expect(cube.activeCount).toBe(0);
+    expect(cube.ticks).toBe(0);
+    expect(cube.bounds).toBeNull();
+  });
+
+  test('Cube constructor with input', () => {
+    const input = ['.#.', '..#', '###'];
+    const cube = new Cube(4, input);
+    expect(cube.ticks).toBe(0);
+    expect(cube.bounds.dimensions).toEqual([3, 3, 1, 1]);
+    expect(cube.activeCount).toBe(5);
+  });
+
+  test('Cube after one tick', () => {
+    const input = ['.#.', '..#', '###'];
+    const cube = new Cube(4, input);
+    cube.tick();
+    expect(cube.ticks).toBe(1);
+    expect(cube.bounds.dimensions).toEqual([5, 5, 3, 3]);
+    expect(cube.activeCount).toBe(29);
+  });
+
+  test('Cube after two ticks', () => {
+    const input = ['.#.', '..#', '###'];
+    const cube = new Cube(4, input);
+    cube.tick();
+    cube.tick();
+    expect(cube.ticks).toBe(2);
+    expect(cube.bounds.dimensions).toEqual([7, 7, 5, 5]);
+    expect(cube.activeCount).toBe(60);
+  });
+});
